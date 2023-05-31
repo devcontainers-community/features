3d87c4cc257a28a5309d3864baec33667f27d498
Jacob Hummer <jcbhmr@outlook.com>
2023-05-31 14:07:56 -0500

    Don't use sudo

--- src/dart/install.sh
+++ src/dart/install.sh
@@ -5,17 +5,16 @@ source _common.sh
 # TODO: Do we need to "check_packages" here?
 
 # https://dart.dev/get-dart
-# "Perform the following one-time setup:"
-sudo apt-get update
-sudo apt-get install apt-transport-https
+apt-get update
+apt-get install apt-transport-https
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-# "Then install the Dart SDK:"
+
 sudo apt-get update
 if [[ -z $VERSION || $VERSION == latest ]]; then
-  sudo apt-get install dart
+  apt-get install dart
 else
-  sudo apt-get install "dart=$VERSION"
+  apt-get install "dart=$VERSION"
 fi
 
 # Now that we have Dart installed to /usr/lib/dart/bin, we need to expose that
