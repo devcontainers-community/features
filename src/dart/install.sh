1cce3c706988dafd3332a56da22e81911cca8d48
Jacob Hummer <jcbhmr@outlook.com>
2023-06-06 00:26:22 -0500

    Move apt deps into check_packages

--- src/dart/install.sh
+++ src/dart/install.sh
@@ -2,11 +2,9 @@
 set -e
 source _common.sh
 
-check_packages wget gpg
+check_packages wget gpg apt-transport-https ca-certificates
 
 # https://dart.dev/get-dart
-apt-get update
-apt-get install apt-transport-https
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
 
