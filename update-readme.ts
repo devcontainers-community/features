6eb07cf5aaff2a843a800aea4ccacbeb836d4d19
Jacob Hummer <jcbhmr@outlook.com>
2023-08-14 16:29:16 -0500

    update readme script

--- update-readme.ts
+++ update-readme.ts
@@ -2,17 +2,27 @@
 import { readFile, writeFile } from "node:fs/promises";
 import { $ } from "npm:zx";
 import process from "node:process";
+import { temporaryDirectory, temporaryWrite } from "npm:tempy";
+import { resolve } from "node:path";
 
-let md = await readFile("README.md", "utf8");
-const collection = `ghcr.io/${process.env.GITHUB_REPOSITORY}:latest`;
-const featureList = JSON.parse(
-  (await $`oras manifest fetch ${collection}`).toString()
+const mdPath = resolve("README.md");
+let md = await readFile(mdPath, "utf8");
+
+const tempDirPath = temporaryDirectory();
+process.chdir(tempDirPath);
+$.cwd = process.cwd();
+
+await $`oras pull ghcr.io/${process.env.GITHUB_REPOSITORY}:latest`;
+const devcontainerCollection = JSON.parse(
+  await readFile("devcontainer-collection.json", "utf8")
 );
-const featureListMD = featureList.features
+
+const featureListMD = devcontainerCollection.features
   .map((f) => `- **[${f.name}](${f.documentationURL})** - ${f.description}`)
   .join("\n");
+
 md = md.replace(
   /<!-- BEGIN_FEATURE_LIST -->([\s\S]*?)<!-- END_FEATURE_LIST -->/,
   `\n${featureListMD}\n`
 );
-await writeFile("README.md", md);
+await writeFile(mdPath, md);
